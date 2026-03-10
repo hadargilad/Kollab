b63273fcf0b45842c62a1ae03cdb9b6f619d7c9b
hadargilad <hadar4224@gmail.com>
2026-03-10 15:17:40 +0200

    add database infrastructure and models

--- AudioIntelDesktop/obj/Release/net10.0-windows/AudioIntelDesktop.AssemblyInfo.cs
+++ AudioIntelDesktop/obj/Release/net10.0-windows/AudioIntelDesktop.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("AudioIntelDesktop")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9217cc2e202660bca6c1de93ef5931a4686f4f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AudioIntelDesktop")]
